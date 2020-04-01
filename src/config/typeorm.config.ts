5c95cb42ead0f1c2b8ccbae2ce38a29430bdb577
tranthaison1231 <thaison.coderstokyo@gmail.com>
2020-04-01 14:10:51 +0700

    update(package): update some package

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ import {
   RDS_PASSWORD,
   RDS_USERNAME,
   RDS_PORT,
-  RDS_HOSTNAME
+  RDS_HOSTNAME,
 } from '../environments';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
@@ -16,18 +16,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: RDS_PASSWORD,
   database: RDS_DB_NAME,
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
-  synchronize: true
+  synchronize: true,
 };
-
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: 'postgres-service',
-//   port: 5432,
-//   username: 'postgres',
-//   password: 'postgres',
-//   database: 'taskmanagement',
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: true
-// };
